82a88cc02ac55351508cd5cb937871a78d72cf47
Samuel <js.rosemond@gmail.com>
2020-06-27 11:22:27 -0400

    code-along clean up setup

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,16 +1,1 @@
-module.exports = {
-  ensureAuth: function (req, res, next) {
-    if (req.isAuthenticated()) {
-      return next()
-    } else {
-      res.redirect('/')
-    }
-  },
-  ensureGuest: function (req, res, next) {
-    if (req.isAuthenticated()) {
-      res.redirect('/dashboard')
-    } else {
-      return next()
-    }
-  },
-}
+module.exports;
